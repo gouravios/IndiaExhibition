005f318e45f8c36c588a085bbc9fad04bae30eeb
nsharma0089 <nikita0089@gmail.com>
2018-03-23 13:20:36 -0700

    login-screen-designing
    screen design

--- IndiaExhibition/Controller/EXHSignInController.swift
+++ IndiaExhibition/Controller/EXHSignInController.swift
@@ -7,17 +7,27 @@
 //
 
 import UIKit
+//import FacebookLogin
 
 class EXHSignInController: UIViewController, Notifiable {
     
+    //Mark: Outlets
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    
     let viewModel = EXHLoginViewModel()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         viewModel.delegate = self
         self.callLoginService()
         // Do any additional setup after loading the view.
+        
+        //Facebook login steps
+        
     }
     
     //MARK: Login Service Call/Registration Service Call..
@@ -28,24 +38,106 @@ class EXHSignInController: UIViewController, Notifiable {
     }
     
     func didLoadData(model:Any?, info:Any?, error:Error?) {
-    
-       // print("service called successfully")
+        
+        // print("service called successfully")
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // MARK: Button outlets
+    
+    func isValidEmail(testStr:String) -> Bool {
+        
+        print("validate emilId: \(testStr)")
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let result = emailTest.evaluate(with: testStr)
+        return result
+        
     }
-    */
-
+    //MARK: IBAction corresponding to Button
+    
+    @IBAction func facebookSignInButtonClicked(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func signInButtonClicked(_ sender: UIButton) {
+        
+        if (txtEmail.text?.isEmpty)! {
+            let alertController = UIAlertController(title: "Alert", message: "Please enter vaild Email", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default)
+            
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+        }
+            
+        else if !isValidEmail(testStr: txtEmail.text!) {
+            
+            let alertController = UIAlertController(title: "Alert", message: "Please enter valid email address.", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default)
+            
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+        }
+            
+        else if (txtPassword.text?.isEmpty)! {
+            let alertController = UIAlertController(title: "Alert", message: "Password field should not be empty", preferredStyle: .alert)
+            let action = UIAlertAction(title: "Ok", style: .default)
+            
+            alertController.addAction(action)
+            self.present(alertController, animated: true, completion: nil)
+            
+        } else {
+            // web services remaining
+            
+        }
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
+//Mark: Text field delegate methods
+extension EXHSignInController : UITextFieldDelegate {
+    
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing")
+        
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        if textField == txtEmail{
+            txtPassword.becomeFirstResponder()
+        }
+        
+        if (textField.returnKeyType == UIReturnKeyType.done) {
+            textField.resignFirstResponder()
+        }
+        
+        print("textFieldShouldReturn")
+        return true
+    }
+    
+    
+    
+    
+}
+
